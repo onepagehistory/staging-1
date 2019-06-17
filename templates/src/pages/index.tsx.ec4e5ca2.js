2c2821f6654f419a70565045845d3f89c82ba54c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-17 18:41:58 +0000

    Deploy onepagehistory/staging-1 to github.com/onepagehistory/staging-1.git:master

--- templates/src/pages/index.tsx.ec4e5ca2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{68:function(n,e,t){"use strict";t.r(e);var a=t(0),u=t.n(a),c=t(35);e.default=function(){return u.a.createElement(c.a,null)}}}]);
